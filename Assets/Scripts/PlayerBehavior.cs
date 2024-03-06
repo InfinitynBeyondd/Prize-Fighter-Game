c1fe72368654e6adfee1b1629b2b9d0a57ceb700
Dylan Torres <glaciershield@gmail.com>
2024-03-06 17:10:21 -0500

    Setup Part 3
    SampleCube prefab has been moved into a new Player prefab, and the PlayerBehavior script has been outlined more thoroughly.

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -15,8 +15,8 @@ public class PlayerBehavior : MonoBehaviour
 
     [Header("Ground Check")]    
     public LayerMask groundLayer; // Layer mask determining what is a ground layer.
-    private bool isGrounded; // Bool that says when the player is on the ground. 
-    private float groundDrag; // The amount of drag experienced on the ground.
+    private bool isGrounded; // Bool that says when the player is on a ground layer. 
+    private float groundDrag; // The amount of drag experienced when moving on the ground.
 
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space; // Jump Key is set to Spacebar by default, but it can be changed in the inspector.
@@ -31,8 +31,16 @@ public class PlayerBehavior : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
+    {        
+        rB = GetComponent<Rigidbody>(); // Gets the rigidbody.
+        rB.freezeRotation = true; // Freeze player's rotation so they don't tilt or fall over.
+
+        // Makes the cursor invisible on screen (will be used in finished release only, not during debugging).
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
         
+        gM = GameObject.Find("GameManager").GetComponent<GameManager>(); // Get reference to GameManager script.
+
     }
 
     // Update is called once per frame
@@ -52,6 +60,12 @@ public class PlayerBehavior : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal"); // Sets the player's horizontal move input.
         verticalInput = Input.GetAxisRaw("Vertical"); // Sets the player's vertical move input.
+
+        // Inputs used to switch between idle animation and moving animation.
+        bool hasHorizontalInput = !Mathf.Approximately(horizontalInput, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(verticalInput, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
     }
 
     void PlayerMovement() 

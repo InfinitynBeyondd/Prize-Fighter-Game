f01e687fff3cbfd01c161334f11aa852c8ffde3c
Dylan Torres <glaciershield@gmail.com>
2024-03-06 01:07:25 -0500

    Basic Script Structures and Programmer Scene Built
    Established guidelines for PlayerBehavior and SceneManager scripts, and established ProgrammerTest scene.

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    // TO DO:
+    // CARLOS: Jump, Basic Movement, Grapple, Double Jump
+    // DYLAN: Grounded Punch, Diving Punch
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayerMovement() 
+    {
+
+    }
+
+    void Jump() 
+    {
+
+    }
+
+    void Grapple() 
+    {
+
+    }
+
+    void Attack() 
+    {
+
+    }
+
+}

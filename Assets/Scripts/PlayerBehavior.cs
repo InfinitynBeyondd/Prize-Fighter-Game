79b5b6301efd174845d469cba0476c3e2ab9aa38
Dylan Torres <glaciershield@gmail.com>
2024-03-06 16:42:17 -0500

    Setup Part 2
    PlayerBehavior script has been updated, a basic animator has been implemented for movement, prefabs for the player and the ground have been built, and a virtual Cinemachine camera has been built to follow the player.

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    // TO DO:
-    // CARLOS: Jump, Basic Movement, Grapple, Double Jump
-    // DYLAN: Grounded Punch, Diving Punch
+    // TO DO: Jump, Basic Movement, Grapple, Double Jump, Grounded Punch, Diving Punch, Wall Jump
+
+    [Header("Player Movement")]
+    private float movementSpeed; // Player's movement speed.
+    private float horizontalInput; // Player's horizontal input.
+    private float verticalInput; // Player's vertical input.
+    private Vector3 moveDirection; // Vector3 determining where player is moving.
+    private Rigidbody rB; // Player's rigidbody.
+
+    [Header("Ground Check")]    
+    public LayerMask groundLayer; // Layer mask determining what is a ground layer.
+    private bool isGrounded; // Bool that says when the player is on the ground. 
+    private float groundDrag; // The amount of drag experienced on the ground.
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space; // Jump Key is set to Spacebar by default, but it can be changed in the inspector.
+
+    [Header("References")]
+    public Transform player;
+    public Transform playerCharacter;
+    public Transform orientation; // The orientation or direction of the player.
+    private float rotationSpeed = 10f; // Speed determining player's rotation.
+    private GameManager gM; // Script reference.
+    [SerializeField] Animator m_Animator; // Player animator.
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +39,19 @@ public class PlayerBehavior : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // FixedUpdate is called once per physics update    
+    void FixedUpdate()
+    {
+
+    }
+
+    // PlayerInput determines when players are inputting movement
+    void PlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal"); // Sets the player's horizontal move input.
+        verticalInput = Input.GetAxisRaw("Vertical"); // Sets the player's vertical move input.
     }
 
     void PlayerMovement() 

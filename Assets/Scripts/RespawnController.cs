b0461e322926209533d3bc5461590d7390ba7569
Dylan Torres <glaciershield@gmail.com>
2024-03-13 23:31:26 -0400

    Checkpoint System Updated
    Updated PlayerBehavior and RespawnController scripts to update the respawn point when a checkpoint is crossed.

--- Assets/Scripts/RespawnController.cs
+++ Assets/Scripts/RespawnController.cs
@@ -6,12 +6,14 @@ public class RespawnController : MonoBehaviour
 {
     [Header("Out of Bounds")]
     [SerializeField] GameObject respawnPoint;
+    public Vector3 pathToRespawn;
 
 
     // Start is called before the first frame update
     void Start()
     {
         respawnPoint = GameObject.FindGameObjectWithTag("Respawn"); // Finds the first object with the respawn tag.
+        pathToRespawn = respawnPoint.transform.position;
     }    
 
     // Once the player collides with anything forcing a respawn, it will be sent back to the level's designated spawn point.
@@ -20,7 +22,7 @@ public class RespawnController : MonoBehaviour
         if (other.transform.CompareTag("Player"))
         {
             Debug.Log("OUT OF BOUNDS - Moving back to spawn point!");
-            other.transform.position = respawnPoint.transform.position;
+            other.transform.position = pathToRespawn;
         }
     }
 

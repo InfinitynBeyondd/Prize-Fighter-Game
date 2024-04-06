c6511f71aba24bec32cce7ae6e07343ba85db72f
Michelle Radcliffe <142946564+littlestdog@users.noreply.github.com>
2024-04-06 10:54:48 -0400

    Impelemented white platform materials in tutorial level and added indicators for dialogue triggers

---
+++ Assets/Scripts/LBPMcamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LBPMcamera : MonoBehaviour
+{
+	public Transform player;
+
+	void Update()
+	{
+		transform.position = new Vector3 (player.position.x, player.position.y + 1.0f , -1.0f);
+	}
+}

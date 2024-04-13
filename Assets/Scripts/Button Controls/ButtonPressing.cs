ac61a10587a77e16e877afbcef7a50435d65c5c8
Dylan Torres <glaciershield@gmail.com>
2024-04-13 17:47:06 -0400

    Buttons Activate Designated Transforms
    Buttons now have a reference to a Transform in the ButtonPressing script. This is meant to be set in the inspector for the buttons, and it will either turn a GameObject tagged "ButtonActivated" off or a GameObject tagged "ButtonDeactivated" on when pressed.

---
+++ Assets/Scripts/Button Controls/ButtonPressing.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPressing : MonoBehaviour
+{
+    [SerializeField] Animator thisButtonAnimator;
+    public bool buttonIsPressed;
+
+    [Header("SET THIS IN THE INSPECTOR!")]
+    [SerializeField] Transform partneredAsset; // Transform affected by whichever button this is. SET THIS IN THE INSPECTOR!
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisButtonAnimator = GetComponentInParent<Animator>();
+        SetPartneredAssetAtStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisButtonAnimator.SetBool("isPressed", buttonIsPressed);
+        ToggleOnOrOff();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Attack")) 
+        {
+            buttonIsPressed = true;            
+        }
+    }
+
+    void SetPartneredAssetAtStart()
+    {
+        if (partneredAsset.CompareTag("ButtonActivated"))
+        {
+            partneredAsset.gameObject.SetActive(false);
+        }
+        else if (partneredAsset.CompareTag("ButtonDeactivated"))
+        {
+            partneredAsset.gameObject.SetActive(true);
+        }
+    }
+
+    void ToggleOnOrOff() 
+    {
+        if (buttonIsPressed && partneredAsset.CompareTag("ButtonActivated"))
+        {
+            //Debug.Log("Button pressed - TURN ON THIS TRANSFORM!");
+            partneredAsset.gameObject.SetActive(true);
+        }
+        else if (buttonIsPressed && partneredAsset.CompareTag("ButtonDeactivated"))
+        {
+            //Debug.Log("Button pressed - TURN OFF THIS TRANSFORM!");
+            partneredAsset.gameObject.SetActive(false);
+        }
+    }
+
+}

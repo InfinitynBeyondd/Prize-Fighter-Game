054478d0bd397c27d6e42934b6cfd2d807f2f60d
Dylan Torres <glaciershield@gmail.com>
2024-04-24 00:11:42 -0400

    Enemy Frog Animations Implemented
    The frogEnemyWithTargets prefab now has a proper death animation.

--- Assets/Scripts/Enemy AI/Frog/FrogHurtbox.cs
+++ Assets/Scripts/Enemy AI/Frog/FrogHurtbox.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class FrogHurtbox : MonoBehaviour
 {
+    [SerializeField] Animator frogAnim;
+    //[SerializeField] GameObject frogEnemyParent;
+
+    private void Awake()
+    {
+        //frogEnemyParent = GetComponentInParent<GameObject>();
+        //frogAnim = GetComponentInParent<Animator>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Attack"))
         {
-            Destroy(transform.parent.gameObject);
+            //Destroy(transform.parent.gameObject);
+            frogAnim.SetBool("isAttacked", true);
             Debug.Log("PACKWATCH");
         }
     }
